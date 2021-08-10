39af8ceedd87a9a01b4c465845cdb1032b3259e6
amit maity <amit@codeclouds.in>
2021-08-10 20:04:44 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+    $(document).ready(function(){
+      $('.sec6 .owl-carousel').owlCarousel({
+          loop:true,
+          margin:10,
+          navigation: true, // Show next and prev buttons
+          slideSpeed: 300,
+          paginationSpeed: 400,
+          singleItem: true,
+          margin: 0,
+          items: 1,
+          autoplay:false,
+          autoplayTimeout: 5000,
+          autoplayHoverPause:true,
+          loop: true,
+          responsiveClass:true,
+          responsive:{
+              0:{
+                  items:1,
+                  nav:true
+              },
+              767:{
+                  items:2,
+                  nav:true
+              }
+          }
+      });
+    });
+
+
